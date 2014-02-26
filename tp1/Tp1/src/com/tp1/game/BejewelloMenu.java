9804a68a19ee96663fcf95c04fbb2c5b967f9fc0
gab <plog2812@gmail.com>
2014-02-25 22:58:23 -0500

    input du user

--- tp1/Tp1/src/com/tp1/game/BejewelloMenu.java
+++ tp1/Tp1/src/com/tp1/game/BejewelloMenu.java
@@ -1,16 +1,21 @@
 package com.tp1.game;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class BejewelloMenu extends Activity
 {	
 	public enum Mode {CHRONO, LIMITE, MODE_NUMBER}
 	static Mode _mode;
+	String _playerName;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,20 +41,55 @@ public class BejewelloMenu extends Activity
         super.onPause();
     }
     
+    public void showDialog()
+    {
+    	// get prompts.xml view
+		LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+		View promptView = layoutInflater.inflate(R.layout.dialog, null);
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+		// set prompts.xml to be the layout file of the alertdialog builder
+		alertDialogBuilder.setView(promptView);
+
+		final EditText input = (EditText) promptView.findViewById(R.id.userInput);
+
+		// setup a dialog window
+		alertDialogBuilder
+				.setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// get user input and set it to result
+								_playerName = input.getText().toString();
+								Intent intent = new Intent(BejewelloMenu.this, Bejewello.class);
+						        startActivity(intent);
+						        finish();
+							}
+						})
+				.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,	int id) {
+								dialog.cancel();
+							}
+						});
+
+		// create an alert dialog
+		AlertDialog alertD = alertDialogBuilder.create();
+
+		alertD.show();
+    }
+    
     public void chronoButtonPressed(View v)
 	{
     	_mode = Mode.CHRONO;
-    	Intent intent = new Intent(this, Bejewello.class);
-        startActivity(intent);
-        finish();
+    	showDialog();    	
 	}
     
     public void limiteButtonPressed(View v)
 	{
     	_mode = Mode.LIMITE;
-    	Intent intent = new Intent(this, Bejewello.class);
-        startActivity(intent);
-        finish();
+    	showDialog();    	
 	}
     
     public void scoresButtonPressed(View v)
